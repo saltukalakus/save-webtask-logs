7a35393c6f09de411a6a6b9468454f9348fdf612
Saltuk Alakus <saltukalakus@gmail.com>
2020-10-05 23:37:48 +0300

    minor change

--- background.js
+++ background.js
@@ -1,27 +1,21 @@
-    chrome.runtime.onMessage.addListener(
-        function(arg, sender, sendResponse) {
-            console.log("RUN BACKGROUND...1");
-            var args=arg.collection;
-            for (i in args){
-            var img_url=args[i];
-            try{
-             saveas=img_url.replace(/[^a-zA-Z0-9]/g,'-');
-            }
-            catch (problem){
-            }
-        
-            console.log("RUN BACKGROUND...2");
-             chrome.downloads.download({
-             url: img_url,
-             filename: saveas,
-             saveAs: false
-            });
-           }
-          console.log(sender.tab ?
-                      "from a content script:" + sender.tab.url :
-                      "from the extension");
+// Store the log files with a background job
+chrome.runtime.onMessage.addListener(
+  function(arg, sender, sendResponse) {
+    var args=arg.collection;
+    for (i in args){
+      var file_url=args[i];
+      try{
+        saveas=file_url.replace(/[^a-zA-Z0-9]/g,'-');
+      }
+      catch (problem){
+      }
 
-
-          if (arg.greeting == "hello")
-            sendResponse({farewell: "goodbye"});
-        });
+      chrome.downloads.download({
+        url: file_url,
+        filename: saveas,
+        saveAs: false
+      });
+    }
+    if (arg.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
--- content.js
+++ content.js
@@ -1,22 +1,29 @@
+// Convert HTLM logs to CVS logs
+function parse(htmlLogs) {
+  var cvsLogs = htmlLogs;
+  return cvsLogs;
+}
+
+// Grab the logs from scree and send to background job
 function transferLog() {
-    console.log("HELLOOO..");
-    var msg = document.getElementsByClassName("wt-logs-list-container")[0].innerHTML.toString();
-    document.getElementsByClassName("wt-icon-261")[0].click();
-    var blob = new Blob( [msg], {type: "text/plain"});
+    
+  // Read the logs from HTML
+  var logs = document.getElementsByClassName("wt-logs-list-container")[0].innerHTML.toString();
+    
+  // Clear the logs with clicking the button
+  document.getElementsByClassName("wt-icon-261")[0].click();
 
-    console.log("BLOB...");
-    console.log(blob.text);
-    var url = URL.createObjectURL(blob);
+  // Keep the logs in the Blob
+  var blob = new Blob( [parse(logs)], {type: "text/plain"});
+  console.log(blob.text);
+  var url = URL.createObjectURL(blob);
 
-    console.log("URL...");
-    console.log(url);
-    //chrome.runtime.sendMessage(url);
-
-    if(typeof chrome.app.isInstalled!=='undefined'){
-      chrome.runtime.sendMessage({greeting: "hello", collection: [url]}, function(response) {
-        console.log(response.farewell);
-      });
-   }
+  // Send the blob to background job
+  if(typeof chrome.app.isInstalled!=='undefined'){
+    chrome.runtime.sendMessage({greeting: "hello", collection: [url]}, function(response) {
+    console.log(response.farewell);
+    });
+  }
 }
 
 var intervalID = setInterval(transferLog, 5000);
